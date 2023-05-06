fb91a483264fa1421cce1e6c1fe35ba1fb9986e8
dpwingle <dpwingle@gmail.com>
2023-05-06 15:44:46 -0400

    Initial commit

---
+++ Code/LibraryManagementSystem/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryManagementSystem
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int PageCount { get; set; }
+        private bool CheckedOut;
+        public List<User> BookHistory { get; set; }
+    }
+}
---
+++ Code/LibraryManagementSystem/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem
+{
+    public class User
+    {
+        public bool Librarian { get; set; }
+        private List<Book> CurrentCheckedOutBooks;
+        private List<Book> BookHistory;
+        private decimal lateFeesDue;
+        public string Username { get; set; }
+
+        private string password;
+
+        public string GetPassword()
+        {
+            return password;
+        }
+
+        public void SetPassword(string value)
+        {
+            password = value;
+        }
+
+        public decimal getLateFees()
+        {
+            return lateFeesDue;
+        }
+
+        public void PayLateFees(decimal paymentAmount)
+        {
+            if (paymentAmount <= this.lateFeesDue && paymentAmount > 0) 
+            {
+                this.lateFeesDue = this.lateFeesDue - paymentAmount;
+            }            
+            else
+            {
+                //return error
+            }
+        }
+
+        public List<Book> GetUsersCheckedOutBooks()
+        {
+            return this.CurrentCheckedOutBooks;
+        }
+
+        public void UserCheckedOutBook(Book book)
+        {
+            this.CurrentCheckedOutBooks.Add(book);
+            this.BookHistory.Add(book);
+        }
+
+        public void UserCheckedInBook(Book book)
+        {
+            this.CurrentCheckedOutBooks.Remove(book);
+        }
+    }
+}
